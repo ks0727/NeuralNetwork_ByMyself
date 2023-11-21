aa6f1061220a13f3fbdc6af4eece32784d1d72db
ks0727 <kouske51@icloud.com>
2023-11-21 11:16:45 +0900

    added Layers.pt

---
+++ Layers.py
@@ -0,0 +1,102 @@
+import numpy as np
+import functions
+
+#乗算レイヤ
+class Multiplication_Layer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+    
+    def forward(self,x,y):
+        self.x = x
+        self.y = y
+        out = x*y
+        return out
+    
+    def backward(self,dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx,dy
+
+#和算レイヤ
+class Addition_Layer:
+    def __init__(self):
+        pass
+
+    def forward(self,x,y):
+        out = x+y
+        return out
+    
+    def backward(self,dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx,dy
+    
+#Reluレイヤ
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self,x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    
+    def backward(self,dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+#Sigmoidレイヤ
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+    
+    def forward(self,x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self,dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+    
+#Affineレイヤ
+class Affine:
+    def __init__(self,W,b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self,x):
+        self.x = x
+        out = np.dot(x,self.W) + self.b
+        return out
+    
+    def backward(self,dout):
+        dx = np.dot(dout,self.W.T)
+        self.dW = np.dot(self.x.T,dout)
+        self.db = np.sum(dout,axis=0)
+        return dx
+    
+#Softmaxと交差エントロピーレイヤ
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+    
+    def forward(self,x,t):
+        self.t = t
+        self.y = functions.softmax(x)
+        self.loss = functions.closs_entropy_error(self.y,self.t)
+        return self.loss
+    
+    def backward(self,dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y-self.t)/batch_size
+        return dx
+
